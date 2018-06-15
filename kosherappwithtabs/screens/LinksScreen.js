318952d90813cab01a37e981c3ba9416a0c6b736
Shimon Cohen <S.cohen96@gmail.com>
2018-06-15 12:16:02 -0400

    Fixed Titles to look pretty

--- kosherappwithtabs/screens/LinksScreen.js
+++ kosherappwithtabs/screens/LinksScreen.js
@@ -20,35 +20,81 @@ export default class LinksScreen extends React.Component {
     }
   };
 
-  selectionList = () => {
-    return([
-      {data: [{title: "Kof-K"}, {title: "OU"}, {title: "Star K"}], key: "NY"},
-      {data: [{title: 13}, {title: "OK"}], key: "NJ"},
-    ])
-  }
-
   render() {
-    return (
 
-    <ScrollView style = {styles.container}>
-      <Card containerStyle={{padding: 0}} >
+    const agencies = [
         {
-          agencies.map((u, i) => {
-            return (
-              <ListItem
-              key={i}
-              roundAvatar style = {styles.picHeight}
-              rightTitle={u.name}
-              rightTitleStyle = {styles.rightTitle}
-              subtitle = {u.phone}
-              avatar={{uri:u.avatar}}
-              />
-            );
-          })
-        }
-      </Card>
-    </ScrollView>
-        )
+        name: "Orthodox Union",
+        city: "Teaneck",
+        avatar: 'https://oukosher.org/content/uploads/2012/11/OU-Logo.jpg',
+        phone: "(212) 563-4000"
+        },
+        {
+          name: "Kof-K",
+          city: "Teaneck",
+          avatar: 'http://s3.amazonaws.com/shmoogle/wp-content/uploads/kof-k.gif',
+          phone: "(201) 837-0500"
+        },
+        {
+          name: "OK",
+          city: "Teaneck",
+          avatar: 'https://www.jewishlinknj.com/images/news/2015/93_aug06/44.jpg',
+          phone: "(718) 756-7500"
+        },
+        {
+          name: "Igud Hakashrus of Los Angeles",
+          city: "Los Angeles",
+          avatar: 'http://www.koshermarks.com/wp-content/uploads/2013/06/C-K.jpg',
+          phone : "(323) 935-8383"
+        },
+        {
+          name: "Vaad Hakashrus of Northern California",
+          city: "Los Angeles",
+          avatar: 'https://static.wixstatic.com/media/4676c8_3687292b22c447b0b07f190dcbb76721~mv2.jpg/v1/fill/w_243,h_143,al_c,q_80/4676c8_3687292b22c447b0b07f190dcbb76721~mv2.webp',
+          phone : "(510) 843-8223"
+        },
+        {
+          name: "RCC",
+          city: "Los Angeles",
+          avatar: 'http://www.koshermarks.com/wp-content/uploads/2013/06/rcc_logo.jpg',
+          phone : "213-389-3382"
+        },
+        {
+          name: "Rabbinical council of Orange County and Long Beach",
+          city: "Los Angeles",
+          avatar: 'http://www.vaad.org/wp-content/uploads/2013/02/Rabbinical-Council-of-Orange-County-Long-Beach.jpg',
+          phone : "562- 426-4894"
+        },
+        {
+          name: "Kosher Supervision of America ",
+          city: "Los Angeles",
+          avatar: 'https://i.vimeocdn.com/portrait/5679067_300x300',
+          phone : "310-282-0444"
+        },
+      ];
+
+
+      return (
+
+      <ScrollView style = {styles.container}>
+        <Card containerStyle={{padding: 0}} >
+          {
+            agencies.map((u, i) => {
+              return (
+                <ListItem
+                key={i}
+                title = {u.name}
+                titleStyle = {styles.rightTitle}
+                subtitle = {u.phone}
+                leftSubtitle = {u.city}
+                avatar={{uri:u.avatar}}
+                />
+              );
+            })
+          }
+        </Card>
+      </ScrollView>
+          )
     }
 
 
