68ad74a9ab17b1df311ecee67d03d76142b4d915
Shimon Cohen <S.cohen96@gmail.com>
2018-06-18 10:04:41 -0400

    Created separate arrays for the separate cities

--- kosherappwithtabs/screens/LinksScreen.js
+++ kosherappwithtabs/screens/LinksScreen.js
@@ -21,81 +21,111 @@ export default class LinksScreen extends React.Component {
   };
 
   render() {
-
-    const agencies = [
-        {
-        name: "Orthodox Union",
-        city: "Teaneck",
-        avatar: 'https://oukosher.org/content/uploads/2012/11/OU-Logo.jpg',
-        phone: "(212) 563-4000"
-        },
-        {
-          name: "Kof-K",
-          city: "Teaneck",
-          avatar: 'http://s3.amazonaws.com/shmoogle/wp-content/uploads/kof-k.gif',
-          phone: "(201) 837-0500"
-        },
-        {
-          name: "OK",
-          city: "Teaneck",
-          avatar: 'https://www.jewishlinknj.com/images/news/2015/93_aug06/44.jpg',
-          phone: "(718) 756-7500"
-        },
-        {
-          name: "Igud Hakashrus of Los Angeles",
-          city: "Los Angeles",
-          avatar: 'http://www.koshermarks.com/wp-content/uploads/2013/06/C-K.jpg',
-          phone : "(323) 935-8383"
-        },
-        {
-          name: "Vaad Hakashrus of Northern California",
-          city: "Los Angeles",
-          avatar: 'https://static.wixstatic.com/media/4676c8_3687292b22c447b0b07f190dcbb76721~mv2.jpg/v1/fill/w_243,h_143,al_c,q_80/4676c8_3687292b22c447b0b07f190dcbb76721~mv2.webp',
-          phone : "(510) 843-8223"
-        },
-        {
-          name: "RCC",
-          city: "Los Angeles",
-          avatar: 'http://www.koshermarks.com/wp-content/uploads/2013/06/rcc_logo.jpg',
-          phone : "213-389-3382"
-        },
-        {
-          name: "Rabbinical council of Orange County and Long Beach",
-          city: "Los Angeles",
-          avatar: 'http://www.vaad.org/wp-content/uploads/2013/02/Rabbinical-Council-of-Orange-County-Long-Beach.jpg',
-          phone : "562- 426-4894"
-        },
-        {
-          name: "Kosher Supervision of America ",
-          city: "Los Angeles",
-          avatar: 'https://i.vimeocdn.com/portrait/5679067_300x300',
-          phone : "310-282-0444"
-        },
-      ];
-
-
-      return (
-
-      <ScrollView style = {styles.container}>
-        <Card containerStyle={{padding: 0}} >
+      const NY = [
           {
-            agencies.map((u, i) => {
-              return (
-                <ListItem
-                key={i}
-                title = {u.name}
-                titleStyle = {styles.rightTitle}
-                subtitle = {u.phone}
-                leftSubtitle = {u.city}
-                avatar={{uri:u.avatar}}
-                />
-              );
-            })
-          }
-        </Card>
-      </ScrollView>
-          )
-    }
+          name: "Orthodox Union",
+          city: "Teaneck",
+          avatar: 'https://oukosher.org/content/uploads/2012/11/OU-Logo.jpg',
+          phone: "(212) 563-4000"
+          },
+          {
+            name: "Kof-K",
+            city: "Teaneck",
+            avatar: 'http://s3.amazonaws.com/shmoogle/wp-content/uploads/kof-k.gif',
+            phone: "(201) 837-0500"
+          },
+          {
+            name: "OK",
+            city: "Teaneck",
+            avatar: 'https://www.jewishlinknj.com/images/news/2015/93_aug06/44.jpg',
+            phone: "(718) 756-7500"
+          },
+          {
+            name: "Khal Adath Jeshurun",
+            city: "Teaneck",
+            avatar: 'http://www.crcweb.org/images/images_agencies/0141~KAJ.jpg',
+            phone: "845-425-9089"
+          },
+        ];
+        const California = [
+          {
+            name: "Igud Hakashrus of Los Angeles",
+            city: "Los Angeles",
+            avatar: 'http://www.fairdinks.com.au/product_images/uploaded_images/kosher-symbol-usa08-kehilla-kosher.jpg',
+            phone : "(323) 935-8383"
+          },
+          {
+            name: "Vaad Hakashrus of Northern California",
+            city: "Los Angeles",
+            avatar: 'https://static.wixstatic.com/media/4676c8_3687292b22c447b0b07f190dcbb76721~mv2.jpg/v1/fill/w_243,h_143,al_c,q_80/4676c8_3687292b22c447b0b07f190dcbb76721~mv2.webp',
+            phone : "(510) 843-8223"
+          },
+          {
+            name: "RCC",
+            city: "Los Angeles",
+            avatar: 'http://www.koshermarks.com/wp-content/uploads/2013/06/rcc_logo.jpg',
+            phone : "213-389-3382"
+          },
+          {
+            name: "Rabbinical council of Orange County and Long Beach",
+            city: "Los Angeles",
+            avatar: 'http://www.vaad.org/wp-content/uploads/2013/02/Rabbinical-Council-of-Orange-County-Long-Beach.jpg',
+            phone : "562- 426-4894"
+          },
+          {
+            name: "Kosher Supervision of America ",
+            city: "Los Angeles",
+            avatar: 'https://i.vimeocdn.com/portrait/5679067_300x300',
+            phone : "310-282-0444"
+          },
+        ];
+
+
+    return (
+     <ScrollView style = {styles.container}>
+       <Card containerStyle={{padding: 5}}
+       title = "New York"
+       titleStyle = {styles.titleStyles}>
+         {
+           NY.map((u, i) => {
+             return (
+               <ListItem
+               key={i}
+               title = {u.name}
+               titleStyle = {styles.rightTitle}
+               subtitle = {u.phone}
+               leftSubtitle = {u.city}
+               avatar={{uri:u.avatar}}
+               />
+             );
+           })
+         }
+       </Card>
+
+       //separate cards for different states
+       <Card
+       containerStyle={{padding: 5}}
+       title = "California"
+       titleStyle = {styles.titleStyles}>
+         {
+           California.map((u, i) => {
+             return (
+               <ListItem
+               key={i}
+               title = {u.name}
+               titleStyle = {styles.rightTitle}
+               subtitle = {u.phone}
+               leftSubtitle = {u.city}
+               avatar={{uri:u.avatar}}
+               />
+             );
+           })
+         }
+       </Card>
+     </ScrollView>
+         )
+     }
+ }
 
 
 
@@ -154,7 +184,8 @@ export default class LinksScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
+    paddingTop: 20,
+    paddingBottom: 60,
     backgroundColor: '#fff',
   },
   container1:{
@@ -229,10 +260,19 @@ sectionHeader: {
   fontSize: 14,
   fontWeight: 'bold',
   backgroundColor: 'rgba(247,247,247,1.0)',
-},
-item: {
-  padding: 10,
-  fontSize: 18,
-  height: 44,
-},
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  titleStyles:{
+    fontSize: 28,
+    color: '#0C36C8',
+    alignItems: 'center',
+  },
+  rightTitle:{
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
 });
